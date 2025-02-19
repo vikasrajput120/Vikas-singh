13dc590664ba2df5e4d4682eb198fdf57c45136d
vikasrajput120 <vk594650659@gmail.com>
2025-02-19 16:39:36 +0530

    Update 0tea.js

--- Priyansh/commands/0tea.js
+++ Priyansh/commands/0tea.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         attachment: fs.createReadStream(__dirname + `/noprefix/tea.mp4`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("☕", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🤤", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
