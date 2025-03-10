552a83306e8a058b59e8a01c65d5db7e07be7766
vikasrajput120 <vk594650659@gmail.com>
2025-03-11 00:40:23 +0530

    Update MENTION.js

--- Priyansh/commands/MENTION.js
+++ Priyansh/commands/MENTION.js
@@ -9,8 +9,8 @@ module.exports.config = {
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "100081181726230") {
-    var aid = ["100081181726230"];
+  if (event.senderID !== "100081181726230","100090065722539") {
+    var aid = ["100081181726230","100090065722539"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
       var msg = ["Abee Mere se baat Kar ..use mention kyu kar rhe ho😒", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy h😐🙄", "Tujhe sunai nhi deta mere admin ko tang na kr😒😒", "Mere boss ko tang naa karo", "Abee Bola na mere Admin ko bar bar mention mat karo ek bar me nahi smjh nahi ata", "Kya kaam hai mujhe bol", "Ek Baar me samjh ni ata , kyu mention kar rhe ho use" , "Mere Boss ko tang mt kiya kro smjhe😒"];
